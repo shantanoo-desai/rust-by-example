a03fad0e870925763e37aa0bdbcda6240700f013
Shantanoo Desai <shantanoo.desai@gmail.com>
2021-03-06 22:35:34 +0100

    chore: add main.rs

---
+++ src/main.rs
@@ -0,0 +1,30 @@
+mod chapter1;
+
+mod chapter2;
+
+fn main() {
+    // //  Chapter 1
+    // chapter1::hello::call_hello();
+
+    // //  Chapter 1.2
+    // chapter1::formatted_print::call_formatted();
+
+    // //  Chapter 1.2.1
+    // chapter1::formatted_debug::call_formatted_debug();
+
+    // //  Chapter 1.2.2
+    // chapter1::formatted_display::call_formatted_display();
+
+    // //  Chapter 1.2.2.1
+    // chapter1::formatted_display_list::call_formatted_display_list();
+
+    // //  Chapter 1.2.3
+    // chapter1::formatted_formatting::call_formatted_formatting();
+
+    //   Chapter 2.1
+    chapter2::primitives::call_primitives();
+
+    chapter2::tuples::call_tuples();
+
+    chapter2::arrays_slices::call_array_slices();
+}

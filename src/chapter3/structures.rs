2d5d31351074a0cb9d413eb4b662fdfa0df4dc5b
Shantanoo Desai <shantanoo.desai@gmail.com>
2021-03-09 15:50:29 +0100

    refactor(structs): change logic for square fn

--- src/chapter3/structures.rs
+++ src/chapter3/structures.rs
@@ -29,36 +29,41 @@ impl fmt::Display for Point {
 
 ////////////////////////////////////////////////////////////////////////
 struct Rectangle {
-    p1: Point,
-    p2: Point,
+    top_left: Point,
+    right_bottom: Point,
 }
 
 impl fmt::Display for Rectangle {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{}\n{}", self.p1, self.p2)
+        write!(f, "{}\n{}", self.top_left, self.right_bottom)
     }
 }
 
+#[allow(dead_code)]
 fn area_rect(rec: Rectangle) -> f32 {
     match rec {
         Rectangle {
-            p1: Point { x: x1, y: y1 },
-            p2: Point { x: x2, y: y2 },
+            top_left: Point { x: x1, y: y1 },
+            right_bottom: Point { x: x2, y: y2 },
         } => f32::abs((x2 - x1) * (y1 - y2)),
     }
 }
 
-fn square(ref_point: Point, width: f32) -> Rectangle {
-    let side = Point {
-        x: ref_point.x + width,
-        y: ref_point.y + width,
-    };
+#[allow(dead_code)]
+fn square(lower_left: Point, width: f32) -> Rectangle {
+    
+    // lower_left is (x1, y2) 
+    // x1 + width => (x2, y2) => bottom_right
+    // y2 + width => (x1, y1) => top_left
+    let derived_top_left: Point = Point{ x: lower_left.x , y: lower_left.y + width };
+    let derived_right_bottom: Point =  Point { x: lower_left.x + width, y: lower_left.y };
     Rectangle {
-        p1: ref_point,
-        p2: side,
+        top_left: derived_top_left,
+        right_bottom: derived_right_bottom,
     }
 }
 
+#[allow(dead_code)]
 pub fn call_structures() {
     println!("");
     println!("Chapter 3.1 Structs");
@@ -82,8 +87,8 @@ pub fn call_structures() {
     let rect_top_left = Point { x: 20.0, y: 10.0 };
     let rect_bottom_right = Point { x: 30.0, y: 5.0 };
     let rect: Rectangle = Rectangle {
-        p1: rect_top_left,
-        p2: rect_bottom_right,
+        top_left: rect_top_left,
+        right_bottom: rect_bottom_right,
     };
     println!("Area of Rectangle: {}", area_rect(rect));
 
